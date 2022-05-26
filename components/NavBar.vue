29a6414e46095926922271c04b8d77d87b59db5b
Sadiyah Ali <sadioali@gmail.com>
2022-05-25 17:05:02 -0700

    rename temp file

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,6 +3,7 @@
     <div>
         <NuxtLink to="/about">About</NuxtLink> 
         <NuxtLink to="/contact">Contact</NuxtLink> 
+        <NuxtLink to="/postrecipe">Posts</NuxtLink> 
         <NuxtLink to="/recipes">Recipes</NuxtLink> 
     </div>
 </div>
