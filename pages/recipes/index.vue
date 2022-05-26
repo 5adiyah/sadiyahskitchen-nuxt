082696d6b7a9365e0a92b6f52f4ee9ff097278df
Sadiyah Ali <sadioali@gmail.com>
2022-05-25 17:05:34 -0700

    Attempt to pull in data - not working

---
+++ pages/recipes/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+      <h1>Recipes Page</h1>
+        {{
+            !!recipes?.data[0]?.attributes?.Title
+            ? recipes.data[0].attributes.Title
+            : `The query did not work`
+        }}
+  </div>
+</template>
+
+<script>
+import { allRecipesQuery } from '~/graphql/query';
+
+export default {
+    name: 'Recipes',
+
+    data(){
+        return {
+            recipes: [],
+        }
+    },
+
+    apollo: {
+        recipes: {
+            prefetch: true,
+            query: allRecipesQuery,
+        }
+    },
+
+    head() {
+        return {
+            title: 'Recipes',
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Recipes List for Sadiyahs Kitchen"
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
